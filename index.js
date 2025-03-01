df89acd07e983e948a0b74caf9d43f564d95fc5b
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-01 23:00:52 +0600

    Add client side deployed link.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://link-sharing-system-client.web.app",
+      "https://link-sharing-system-client.firebaseapp.com",
+    ],
     credentials: true,
   })
 );

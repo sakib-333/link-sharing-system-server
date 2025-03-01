5f8ab2de13d6cf3f01794c5c31b12fabda8add9f
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-01 17:28:48 +0600

    Add mongoose to connect database.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,19 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+const db_username = process.env.db_username;
+const db_password = process.env.db_password;
+
+const uri = process.env.URI.replace("<db_username>", db_username).replace(
+  "<db_password>",
+  db_password
+);
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send(
